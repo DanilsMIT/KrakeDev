e649ef0ad025d114e72de5d658ba2c7d27bef906
DanilsMIT <danilsmitdm@gmail.com>
2024-08-06 15:26:46 -0500

    Funcion INT y Funcion Float added

--- utilitarios/saludos.js
+++ utilitarios/saludos.js
@@ -10,4 +10,20 @@ recuperarcmptexto=function(idtxtcomponente){
 saludos=function(){
     let caja=recuperarcmptexto("boxtxt")
     let cajaA=recuperarcmptexto("boxtxtA")
+    let cajaB=recuperarcmpINT("boxtxtB")
+    let cajaC=recuperarcmpFLOAT("boxtxtC")
+
+    console.log(caja,cajaA,cajaB,cajaC)
+}
+
+recuperarcmpINT=function(idNIcomponente){
+    let recovertxtN = recuperarcmptexto(idNIcomponente);
+    let txtINT= parseInt(recovertxtN)
+    return txtINT
+}
+
+recuperarcmpFLOAT=function(idNFcomponente){
+    let recovertxtNF= recuperarcmptexto(idNFcomponente);
+    let txtFLOAT=parseFloat(recovertxtNF)
+    return txtFLOAT
 }
