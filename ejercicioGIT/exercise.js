bdaa38b0adecd6ee20ad87a900002ddfdf4e0a9b
DanilsMIT <danilsmitdm@gmail.com>
2024-08-06 13:27:02 -0500

    boton cambia el GIF

--- ejercicioGIT/exercise.js
+++ ejercicioGIT/exercise.js
@@ -1,3 +1,7 @@
 saluditos=function(){
     console.log("que mas ve")
+    
+    let recoverpicture
+    recoverpicture= document.getElementById("ppgato")
+    recoverpicture.src="./pictures/gatogolpes.gif"
 }
