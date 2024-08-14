50ef8bc7c4236b11e22afa582500dd025e39a1ac
DanilsMIT <danilsmitdm@gmail.com>
2024-08-13 19:32:48 -0500

    Reto 37, bucles MostrarImpares

---
+++ Bucles/bucles.js
@@ -0,0 +1,19 @@
+mostrarNumbers=function(){
+    console.log("before 'bucle for'")
+
+    for(let i=0;i<4;i++){
+        console.log(i)
+    }
+    console.log("after 'bucle for'")
+}
+
+mostrarImpares=function(){
+    console.log("before 'bucle for'")
+
+    for(i=1;i<=21;i+=2){
+        console.log(i)
+    }
+
+    console.log("after 'bucle for'")
+    
+}
