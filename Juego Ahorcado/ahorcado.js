6abef8c9dde17a524c29a42d4a711138a7ef2c40
DanilsMIT <danilsmitdm@gmail.com>
2024-08-14 17:55:29 -0500

    RETO 40, MEJORADO

--- Juego Ahorcado/ahorcado.js
+++ Juego Ahorcado/ahorcado.js
@@ -1,14 +1,19 @@
 let secretWord;
-let intentos = 0;
+let intentos = 10;
 let coincidencias = 0;
 let errores = 0;
 
-let letra1=""
-let letra2=""
-let letra3=""
-let letra4=""
-let letra5=""
+let letra1 = 0;
+let letra2 = 0;
+let letra3 = 0;
+let letra4 = 0;
+let letra5 = 0;
 
+let letraA = "";
+let letraB = "";
+let letraC = "";
+let letraD = "";
+let letraE = "";
 let victoria
 
 esMayuscula = function (letra) {
@@ -50,8 +55,24 @@ guardarPalabra = function () {
         longValida = false
     }
 
-
+    let caractersave
     if (palabraValida == true && longValida == true) {
+        for (n = 0; n < wordLong; n++) {
+            caractersave = word.charAt(n)
+
+            if (n == 0) {
+                letraA = caractersave
+            } else if (n == 1) {
+                letraB = caractersave
+            } else if (n == 2) {
+                letraC = caractersave
+            } else if (n == 3) {
+                letraD = caractersave
+            } else if (n == 4) {
+                letraE = caractersave
+            }
+        }
+
         secretWord = word
         console.log(secretWord)
     } else {
@@ -61,26 +82,26 @@ guardarPalabra = function () {
     }
 }
 
-showLetra = function (letra, posicion) {
+showLetra = function (letraC, posicion) {
 
 
     if (posicion == 0) {
-        mostrarTexto("div0", letra)
-        letra1=letra
-    } else if (posicion == 1) {
-        mostrarTexto("div1", letra)
-        letra2=letra
-    } else if (posicion == 2) {
-        mostrarTexto("div2", letra)
-        letra3=letra
-    } else if (posicion == 3) {
-        mostrarTexto("div3", letra)
-        letra4=letra
-    } else if (posicion == 4) {
-        mostrarTexto("div4", letra)
-        letra5=letra
-    }
+        mostrarTexto("div0", letraC)
+        letra1 = letra1 + 1
 
+    } else if (posicion == 1) {
+        mostrarTexto("div1", letraC)
+        letra2 = letra2 + 1
+    } else if (posicion == 2) {
+        mostrarTexto("div2", letraC)
+        letra3 = letra3 + 1
+    } else if (posicion == 3) {
+        mostrarTexto("div3", letraC)
+        letra4 = letra4 + 1
+    } else if (posicion == 4) {
+        mostrarTexto("div4", letraC)
+        letra5 = letra5 + 1
+    }
 }
 
 validarLetra = function (letra) {
@@ -94,15 +115,12 @@ validarLetra = function (letra) {
         if (letra == compararLetra) {
             showLetra(letra, a)
             coincidencias = coincidencias + 1
-            a = 5
         }
+
 
     }
 
-    if (letra != compararLetra) {
-        alert("La letra no se encuentra en la palabra")
-        errores = errores + 1
-    } 
+
 }
 
 IngresarLetra = function () {
@@ -113,14 +131,25 @@ IngresarLetra = function () {
     } else { alert("¡SOLO SE JUEGA CON LETRAS MAYÚSCULAS!") }
 
     if (coincidencias == 5) {
-        victoria=true
-    } else if (intentos == 10) {
-        victoria=false
+        victoria = true
+        errores = 0
+    } else if (intentos==0) {
+        victoria = false
+        errores = 11
+
+    }
+
+    if (letra == letraA || letra == letraB || letra == letraC || letra == letraD || letra == letraE) {
+    } else {
+        alert("La letra no pertenece a la palabra")
+        errores = errores + 1
     }
 
 
+    intentos = intentos - 1
+    mostrarTexto("intentos", intentos)
 
-    intentos = intentos + 1
+    LetraRepetida()
     mostrarAhorcado()
 
 }
@@ -150,13 +179,103 @@ mostrarAhorcado = function () {
     }
     else if (errores == 9) {
         mostrarImagen("ahorcadoImagen", "./Ahorcado_09.png")
-    }else if(victoria==true){
+    } else if (victoria == true) {
         alert("¡GANASTE!")
         mostrarImagen("ahorcadoImagen", "./ganador.gif")
-    }else if(victoria==false){
+    } else if (victoria == false && errores>10) {
         console.log("PERDISTE...")
         mostrarImagen("ahorcadoImagen", "./gameOver.gif")
         mostrarTexto("palabraguardada", secretWord)
     }
 }
+
+
+
+LetraRepetida = function () {
+
+    if (letra1 >= 2 && letra2 >= 2) {
+        coincidencias = coincidencias - 2
+        letra1 = letra1 - 1
+        letra2 = letra2 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra1 >= 2 && letra3 >= 2) {
+        coincidencias = coincidencias - 2
+        letra1 = letra1 - 1
+        letra3 = letra3 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra1 >= 2 && letra4 >= 2) {
+        coincidencias = coincidencias - 2
+        letra1 = letra1 - 1
+        letra4 = letra4 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra1 >= 2 && letra5 >= 2) {
+        coincidencias = coincidencias - 2
+        letra1 = letra1 - 1
+        letra5 = letra5 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra2 >= 2 && letra3 >= 2) {
+        coincidencias = coincidencias - 2
+        letra2 = letra2 - 1
+        letra3 = letra3 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra2 >= 2 && letra4 >= 2) {
+        coincidencias = coincidencias - 2
+        letra2 = letra2 - 1
+        letra4 = letra4 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra2 >= 2 && letra5 >= 2) {
+        coincidencias = coincidencias - 2
+        letra2 = letra2 - 1
+        letra5 = letra5 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra3 >= 2 && letra4 >= 2) {
+        coincidencias = coincidencias - 2
+        letra3 = letra3 - 1
+        letra4 = letra4 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra3 >= 2 && letra5 >= 2) {
+        coincidencias = coincidencias - 2
+        letra3 = letra3 - 1
+        letra5 = letra5 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra4 >= 2 && letra5 >= 2) {
+        coincidencias = coincidencias - 2
+        letra4 = letra4 - 1
+        letra5 = letra5 - 1
+        alert("Ya se ingreso esta letra")
+    } else if (letra1 >= 2) {
+        coincidencias = coincidencias - 1
+        letra1 = letra1 - 1
+        alert("Ya se ingreso esta letra")
+        letritaA = true
+    } else if (letra2 >= 2) {
+        coincidencias = coincidencias - 1
+        letra2 = letra2 - 1
+        alert("Ya se ingreso esta letra")
+        letritaB = true
+
+    } else if (letra3 >= 2) {
+        coincidencias = coincidencias - 1
+        letra3 = letra3 - 1
+        alert("Ya se ingreso esta letra")
+        letritaC = true
+
+    } else if (letra4 >= 2) {
+        coincidencias = coincidencias - 1
+        letra4 = letra4 - 1
+        alert("Ya se ingreso esta letra")
+        letritaD = true
+
+    } else if (letra5 >= 2) {
+        coincidencias = coincidencias - 1
+        letra5 = letra5 - 1
+        alert("Ya se ingreso esta letra")
+        letritaE = true
+    }
+
+    /////////////////////////////////////////////////////////
+    ///Al ingresar una letra que ya se marco si abarca 1 o 2 espacios no aumenta las coincidencias y suelta el alert
+}
+
+
 
